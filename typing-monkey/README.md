7c3f79c354b5705a9b0efef57f1d728c5b2b568f
Emiliano Dalla Verde Marcozzi <6564766d@gmail.com>
2020-03-14 19:44:45 -0300

    First implementation!

---
+++ typing-monkey/README.md
@@ -0,0 +1,11 @@
+# Infinite Monkey Theorem
+
+The infinite monkey theorem states that a monkey hitting keys at random on a typewriter keyboard for an infinite amount of time will almost surely type any given text, such as the complete works of William Shakespeare.
+
+Here goes my implementation!
+
+## Usage
+
+>>> import monkey
+>>> monkey.score('python', repeat=10000)
+('ykthon', 4)
---
+++ typing-monkey/monkey.py
@@ -0,0 +1,53 @@
+import string
+import random
+
+letters = string.ascii_lowercase + " "
+sheakespeare_phrase = "methinks it is like a weasel"
+
+
+def get_random_char(letters: str) -> str:
+    """Returns a single random char from given letters."""
+    return letters[random.randint(0, len(letters) - 1)]
+
+
+def get_similarity_vector(string1: str, string2: str) -> list:
+    """Returns a list like: [1, 0, 1].
+
+    1 means char at that position on both strings are equal.
+    2 means char at that position on both strings aren't equal.
+    """
+    assert len(string1) == len(string2), "All arguments must have the same length"
+
+    vector = []
+    for idx, ch in enumerate(string1):
+        if string2[idx] == ch:
+            vector.append(1)
+        else:
+            vector.append(0)
+
+    return vector
+
+
+def generate(length: int) -> str:
+    """Returns a random string of given length."""
+    text = ""
+    while len(text) < length:
+        text += get_random_char(letters)
+    return text
+
+
+def score(phrase: str, repeat: int = 1000) -> tuple:
+    """Returns a tuple like: ('random text', score) """
+    best_phrase = ""
+    best_score = 0
+
+    while repeat > 0:
+        random_text = generate(len(phrase))
+        vector = get_similarity_vector(phrase, random_text)
+        new_score = vector.count(1)
+        if new_score >= best_score:
+            best_score = new_score
+            best_phrase = random_text
+        repeat -= 1
+
+    return (best_phrase, best_score)
